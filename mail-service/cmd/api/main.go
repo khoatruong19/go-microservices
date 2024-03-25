fc5ee46d36ce5db22af2a73c12ebe261baa16a93
khoatruong19 <thpthv.truonghuynhdangkhoa@gmail.com>
2024-03-25 23:51:56 +0700

    feat: mail service

---
+++ mail-service/cmd/api/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+)
+
+const webPort = "80"
+
+type Config struct {
+	Mailer Mail
+}
+
+func main() {
+	app := Config{
+		Mailer: createMail(),
+	}
+
+	log.Printf("Starting broker service on port %s\n", webPort)
+
+	// define http server
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%s", webPort),
+		Handler: app.routes(),
+	}
+
+	// start the server
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Panic(err)
+	}
+}
+
+func createMail() Mail {
+	port, _ := strconv.Atoi(os.Getenv("MAIL_PORT"))
+	return Mail{
+		Domain:      os.Getenv("MAIL_DOMAIN"),
+		Host:        os.Getenv("MAIL_HOST"),
+		Port:        port,
+		Username:    os.Getenv("MAIL_USERNAME"),
+		Password:    os.Getenv("MAIL_PASSWORD"),
+		Encryption:  os.Getenv("MAIL_ENCRYPTION"),
+		FromName:    os.Getenv("FROM_NAME"),
+		FromAddress: os.Getenv("FROM_ADDRESS"),
+	}
+}
